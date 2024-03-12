1e94fb43e4a24045b6a84299ec8c5590cd0f1e8b
miriamGmitterova <gmitterova.miriam@gmail.com>
2024-03-12 13:30:58 +0100

    Zaciatok

---
+++ src/main/java/bakalarka/example/bakalarka/VysetreniaApplication.java
@@ -0,0 +1,13 @@
+package bakalarka.example.bakalarka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VysetreniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VysetreniaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bakalarka/example/bakalarka/VysetreniaApplicationTests.java
@@ -0,0 +1,13 @@
+package bakalarka.example.bakalarka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VysetreniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
