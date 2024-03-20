0e37ab5b6563b90f05f54127d2e5a055dfb5f014
miriamGmitterova <gmitterova.miriam@gmail.com>
2024-03-20 14:06:10 +0100

    zaciatok

---
+++ src/main/java/bakalarka/example/bakalarka/configuration/WebConfig.java
@@ -0,0 +1,20 @@
+package bakalarka.example.bakalarka.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//prevzate z tutorialu
+//https://www.youtube.com/watch?v=5HAzLAnJPKU
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*").allowedOrigins("*");
+    }
+}
